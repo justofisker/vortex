a07c92e1bad3a119ff2a7524655dd954a235634c
Syntaxxor <25652538+Syntaxxor@users.noreply.github.com>
2022-05-11 22:37:03 -0700

    Added delta time and edited transform functions to be more efficient.

--- src/ecs/globals.c
+++ src/ecs/globals.c
@@ -7,3 +7,6 @@ uint32_t VE_G_ComponentSizes[VE_ECS_COMPONENT_MAX] = { 0 };
 const char *VE_G_pComponentNames[VE_ECS_COMPONENT_MAX] = { NULL };
 uint32_t VE_G_ComponentCount = 0;
 VE_SceneT VE_G_CurrentScene = { NULL, 0 };
+uint32_t VE_G_LastTick = 0;
+uint32_t VE_G_DeltaMilliseconds = 0;
+float VE_G_DeltaSeconds = 0.0f;
--- src/ecs/globals.h
+++ src/ecs/globals.h
@@ -13,5 +13,8 @@ extern uint32_t VE_G_ComponentSizes[VE_ECS_COMPONENT_MAX];
 extern const char *VE_G_pComponentNames[VE_ECS_COMPONENT_MAX];
 extern uint32_t VE_G_ComponentCount;
 extern VE_SceneT VE_G_CurrentScene;
+extern uint32_t VE_G_LastTick;
+extern uint32_t VE_G_DeltaMilliseconds;
+extern float VE_G_DeltaSeconds;
 
 #endif //ECS_GLOBALS_H
