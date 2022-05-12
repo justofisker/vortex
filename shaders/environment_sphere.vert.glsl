d333d68c7f1726bbce130f6d28f0f13041801975
Bea <37525404+justofisker@users.noreply.github.com>
2022-05-12 00:04:15 -0700

    Static environment sphere position

--- shaders/environment_sphere.vert.glsl
+++ shaders/environment_sphere.vert.glsl
@@ -16,7 +16,13 @@ layout(location = 1) out vec2 fragTexCoord;
 layout(location = 2) out vec3 fragNorm;
 
 void main() {
-    gl_Position = ubo.proj * ubo.view * vec4(inPosition, 1.0);
+    // Ignore camera translation
+    mat4 newView = ubo.view;
+    newView[3][0] = 0;
+    newView[3][1] = 0;
+    newView[3][2] = 0;
+
+    gl_Position = ubo.proj * newView * vec4(inPosition, 1.0);
     fragColor = inColor;
     fragTexCoord = inTexCoord;
     fragNorm = (ubo.model * vec4(inNormal, 0.0)).xyz;
